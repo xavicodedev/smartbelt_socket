77968a4ca0bff6fa878bc5e4b230c19a74542080
Xavi Garcia <crafttutos@gmail.com>
2022-03-03 09:48:24 +0100

    Added message on site

--- socket.js
+++ socket.js
@@ -6,9 +6,28 @@ var mysql = require('mysql');
 */
 // The port on which the server is listening.
 //const port = 8080;
-//use alternate localhost and the port Heroku assigns to $PORT
+
+//Use alternate localhost and the port Heroku assigns to $PORT
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
+
+var express = require('express');
+var app = express();
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('El servidor service-smartbelt está encendido...');
+})
+
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+  
+  console.log("Example app listening at http://%s:%s", host, port)
+})
+
 /** 
 //Crea la conexión a base de datos.
 //TODO Parametrizar los datos de BBDD por seguridad.
@@ -22,23 +41,26 @@ var con = mysql.createConnection({
 
 // Use net.createServer() in your code. This is just for illustration purpose.
 // Create a new TCP server.
-const server = new Net.Server();
+const netServer = new Net.Server();
 // The server listens to a socket for a client to make a connection request.
 // Think of a socket as an end point.
-server.listen(port, function() {
+netServer.listen(port, function() {
     console.log(`Server listening for connection requests on socket localhost:${port}`);
 });
 
 // When a client requests a connection with the server, the server creates a new
 // socket dedicated to that client.
-server.on('connection', function(socket) {
+netServer.on('connection', function(socket) {
     console.log('A new connection has been established.');
 
 
     // The server can also receive data from the client by reading from its socket.
     socket.on('data', function(chunk) {
         console.log(`Data received from client: ${chunk.toString()}`);
+        
     });
+
+    
 
     // When the client requests to end the TCP connection with the server, the server
     // ends the connection.
@@ -51,6 +73,12 @@ server.on('connection', function(socket) {
         console.log(`Error: ${err}`);
     });
 });
+
+
+
+
+
+
 
 
 //Conecta con BBDD e inserta los datos.
